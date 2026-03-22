bd0c67c27cefde2a1f08efb1b1edb4ede674d586
Novaes <jvn.novaes@gmail.com>
2026-03-22 12:49:34 -0300

    Feat: Implementado o metodo Empilhar() - funcional -, exibir() e metodo construtor;

--- src/Pilha/PilhaDinamica.java
+++ src/Pilha/PilhaDinamica.java
@@ -3,7 +3,21 @@ package Pilha;
 public class PilhaDinamica {
     private No inicio;
 
+    public PilhaDinamica() {
+        this.inicio = null;
+    }
 
+    // Forma a qual Pilhas funcionam: First in - Last out
+    public void empilhar(String conteudo){
+        No aux = new No(conteudo);
+        aux.setProximo(this.inicio);
+        this.inicio = aux;
+    }
+
+    // Forma a qual Pilhas funcionam: First in - Last out
+    public void desempilhar(){
+
+    }
 
 
     public boolean estaVazia(){
@@ -15,6 +29,18 @@ public class PilhaDinamica {
             System.out.println("Pilha vazia!");
             return null;
         }
-        return inicio.conteudo;
+        return inicio.getConteudo();
+    }
+
+    public void exibir(){
+        if(estaVazia()){
+            System.out.println("Pilha está vazia!");
+            return;
+        }
+        No atual = inicio;
+        while(atual != null){
+            System.out.println(atual.getConteudo());
+            atual = atual.getProximo();
+        }
     }
 }
