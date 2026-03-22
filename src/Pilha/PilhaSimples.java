826f33b08a31e762424c28fee17462945f25d579
Novaes <jvn.novaes@gmail.com>
2026-03-22 13:55:29 -0300

    Feat: metodos empilhar(), desempilhar(), estaCheia() e exibir() implementados

--- src/Pilha/PilhaSimples.java
+++ src/Pilha/PilhaSimples.java
@@ -1,4 +1,54 @@
 package Pilha;
 
 public class PilhaSimples {
+    String[] lista;
+    private int topo;
+
+    public PilhaSimples(int tamanho) {
+        if (tamanho <= 0) throw new IllegalArgumentException();
+        lista = new String[tamanho];
+        this.topo = -1;
+    }
+
+    public void empilhar(String x) {
+        if (estaCheia()) {
+            System.out.println("A pilha está cheia!");
+            return;
+        }
+
+        this.topo++;
+        this.lista[this.topo] = x;
+    }
+
+    public String desempilhar() {
+        // Como removemos o método estaVazia, checamos o topo diretamente aqui
+        if (this.topo == -1) {
+            System.out.println("A pilha está vazia!");
+            return null;
+        }
+
+        String itemRemovido = this.lista[this.topo];
+        this.lista[this.topo] = null;
+        this.topo--;
+
+        return itemRemovido;
+    }
+
+    public boolean estaCheia() {
+        return this.topo == this.lista.length - 1;
+    }
+
+    public void exibir() {
+        // Checamos o topo diretamente aqui também
+        if (this.topo == -1) {
+            System.out.println("A pilha está vazia!");
+            return;
+        }
+
+        System.out.println("--- Topo da Pilha ---");
+        for (int i = this.topo; i >= 0; i--) {
+            System.out.println(this.lista[i]);
+        }
+        System.out.println("--- Base da Pilha ---");
+    }
 }
