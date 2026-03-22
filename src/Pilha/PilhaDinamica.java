6e19568d472600b0d6a745b807ddcf34d5e650d8
Novaes <jvn.novaes@gmail.com>
2026-03-22 12:31:04 -0300

    Feat: Implementado os metodos estaVazia() e consultarInicio()

--- src/Pilha/PilhaDinamica.java
+++ src/Pilha/PilhaDinamica.java
@@ -1,4 +1,20 @@
 package Pilha;
 
 public class PilhaDinamica {
+    private No inicio;
+
+
+
+
+    public boolean estaVazia(){
+        return this.inicio == null;
+    }
+
+    public String consultarInicio(){
+        if(estaVazia()){
+            System.out.println("Pilha vazia!");
+            return null;
+        }
+        return inicio.conteudo;
+    }
 }
